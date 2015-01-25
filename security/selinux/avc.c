ab3f208753ce977f9671b2b33c45c06f6bdb262c
Pranav Vashi <neobuddy89@gmail.com>
2015-01-25 23:40:18 +0530

    selinux: Add option to force audit
    * To help debugging selinux
    * echo Y > /sys/module/selinux/parameters/force_audit
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	security/selinux/avc.c

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -23,6 +23,7 @@
 #include <linux/skbuff.h>
 #include <linux/percpu.h>
 #include <linux/list.h>
+#include <linux/moduleparam.h>
 #include <net/sock.h>
 #include <linux/un.h>
 #include <net/af_unix.h>
@@ -108,6 +109,9 @@ struct pantech_avc_format pantech_get_avc(void)
 static struct kmem_cache *avc_xperms_data_cachep;
 static struct kmem_cache *avc_xperms_decision_cachep;
 static struct kmem_cache *avc_xperms_cachep;
+
+static bool force_audit = false;
+module_param(force_audit, bool, 0644);
 
 static inline int avc_hash(u32 ssid, u32 tsid, u16 tclass)
 {
@@ -843,6 +847,7 @@ inline int avc_audit(u32 ssid, u32 tsid,
 		    a->selinux_audit_data->auditdeny &&
 		    !(a->selinux_audit_data->auditdeny & avd->auditdeny))
 			audited = 0;
+			if (force_audit) audited = 1;
 	} else if (result)
 		audited = denied = requested;
 	else
