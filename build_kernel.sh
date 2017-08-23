8e2d38f57de595eafc3ba32c3fad1bcfe0897b41
KaoTD <kaotd99@gmail.com>
2017-08-24 01:12:52 +0700

    update

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,11 +17,12 @@ export CROSS_COMPILE=arm-eabi-
 # make zImage
 ##############################################################################
 mkdir -p ./obj/KERNEL_OBJ/
-make ARCH=arm O=./obj/KERNEL_OBJ/ cyanogenmod_ef63_defconfig
-make -j8 ARCH=arm O=./obj/KERNEL_OBJ/ 2>&1 | tee kernel_log.txt
+make O=./obj/KERNEL_OBJ/ ef59_defconfig
+make -j8 CONFIG_NO_ERROR_ON_MISMATCH=y O=./obj/KERNEL_OBJ/ 2>&1
 
-defconfig support: kd_ef60_defconfig , kd_ef63_defconfig
 make CONFIG_NO_ERROR_ON_MISMATCH=y
+
+arm-eabi-strip -g wlan.ko
 
 ##############################################################################
 # Make dt.img & copy zImage
