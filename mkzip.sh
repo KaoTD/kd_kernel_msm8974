ed74c5298c26a70e2bb6b6db93ceeda56dc96b46
KaoTD <kaotd99@gmail.com>
2017-12-21 13:38:21 +0700

    update mkzip.sh - force copy modules to override old modules copied before - remo strip debug code because Makefile already do that

--- mkzip.sh
+++ mkzip.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 BUILDDATE=`date +%Y%m%d-%H%M`
 ZIP=kdk-vega-$BUILDDATE.zip
-cp drivers/*/*.ko ./
-cp drivers/*/*/*.ko ./
+cp -f drivers/*/*.ko ./
+cp -f drivers/*/*/*.ko ./
 ls ./*.ko > modules.list
 
-~/linaro-arm-eabi-4.9/bin/arm-eabi-strip -g `cat modules.list`
+#~/linaro-arm-eabi-4.9/bin/arm-eabi-strip -g `cat modules.list`
 
 zip $ZIP zImage dt.img `cat modules.list`
 
